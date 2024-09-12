60c6a9dd573c9eafa60f5e9e89f823fe17b10772
leordzgcia <leongarzagarza@hotmail.com>
2024-09-12 16:52:25 -0600

    Clases reposit

--- src/main/java/com/example/demo/repositories/CotizacionRepository.java
+++ src/main/java/com/example/demo/repositories/CotizacionRepository.java
@@ -1,2 +1,10 @@
-package com.example.demo.repositories;public interface CotizacionRepository {
+package com.example.demo.repositories;
+
+import com.example.demo.models.CotizacionModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CotizacionRepository extends CrudRepository<CotizacionModel,Long> {
 }
